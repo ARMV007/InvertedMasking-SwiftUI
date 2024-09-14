92b4714fd14eb3343517fbbd6be393a127320fd2
Abhishek Verma <abhishek.rmv@gmail.com>
2024-09-14 19:30:02 +0530

    Intiail Project

---
+++ SwiftUI_Demo/SwiftUI_Demo/SwiftUI_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DemoApp.swift
+//  SwiftUI_Demo
+//
+//  Created by Siri on 14/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
